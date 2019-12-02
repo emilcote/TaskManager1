832393970e8d296c64415bcb008e8bc38543f8ef
Andreww8xx8 <kupertfail1498@gmail.com>
2019-12-02 19:25:19 +0300

    state_machines-activerecord

--- TaskManager/app/models/task.rb
+++ TaskManager/app/models/task.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
 
@@ -9,6 +8,41 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
-  end
-end
 
+  state_machine initial: :new_task do
+    event :develop do
+      transition in_qa: :in_development
+      transition new_task: :in_development
+      transition in_code_review: :in_development
+    end
+
+    event :start_qa do
+      transition in_development: :in_qa
+    end
+
+    event :finish_qa do
+      transition in_qa: :in_code_review
+    end
+
+    event :complete_review do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :archiv do
+      transition new_task: :archived
+      transition released: :archived
+    end
+
+    event :finish_release do
+      transition ready_for_release: :released
+    end
+  end
+
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :description, length: { maximum: 500 }
+end
