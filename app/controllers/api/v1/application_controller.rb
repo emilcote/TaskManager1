7c7ba00a436b331471806470347f15add70acd4d
Andreww8xx8 <kupertfail1498@gmail.com>
2019-12-18 08:26:48 +0300

    edit AuthHelper

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::ApplicationController < ApplicationController
-  include AuthHelper
-  helper_method :current_user
   def build_meta_tasks(collection)
     {
       count: collection.count,
