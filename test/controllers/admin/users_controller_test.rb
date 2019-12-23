de585d12f9ccfb401e0c188c07a8ff64db8f1883
Andreww8xx8 <kupertfail1498@gmail.com>
2019-12-23 18:25:11 +0300

    edit users_controller_test

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -14,22 +14,26 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     get new_admin_user_url
     assert_response :success
   end
+
   test 'should post create' do
     user = attributes_for(:user)
-    post admin_users_url, params: { user: user }
-    assert_response :redirect
+    assert_difference 'User.count', +1 do
+      post admin_users_url, params: { user: user }
+    end
   end
 
   test 'should patch update' do
     user = create(:user)
     user_attrs = attributes_for(:user)
     patch admin_user_url user.id, params: { user: user_attrs }
-    assert_response :redirect
+    user.reload
+    assert_not_equal user.slice(*user_attrs.keys), user_attrs
   end
 
   test 'should delete destroy' do
     user = create(:user)
-    delete admin_user_url user.id
-    assert_response :redirect
+    assert_difference 'User.count', -1 do
+      delete admin_user_url user.id
+    end
   end
 end
